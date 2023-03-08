7b2b67f78a267e0e94e0dff57e26266f35bbe222
Daniel <Daniel@DESKTOP-T22UJG8>
2023-03-07 20:45:57 -0700

    Modified for netflify submision

--- portfolio-site/src/components/Contact.jsx
+++ portfolio-site/src/components/Contact.jsx
@@ -61,6 +61,7 @@ export default function Contact() {
                 <h2 className="text-lime-600 text-5xl font-bold my-3">Contact</h2>
                 <p className="text-slate-100 my-3">I'm interested in any kind of opportunities. :)</p>
                 <form name="contact" action="" className="flex flex-col" netlify>
+                    <input type="hidden" name="form-name" value="contact" />
                     <div className="flex flex-col 2xl:flex-row justify-between w-full my-5">
                         <input type="text" onChange={handleChange} name="name" value={formData.name} placeholder="Name" className="mb-10 2xl:mb-0 2xl:w-[48%] h-10 p-3 bg-[#2b2b2b] text-slate-100"></input>
                         <input type="text" onChange={handleChange} name="email" value={formData.email} placeholder="Your Email" className="2xl:w-[48%] h-10 p-3 bg-[#2b2b2b] text-slate-100"></input>
@@ -68,7 +69,6 @@ export default function Contact() {
                     <input type="text" onChange={handleChange} name="subject" value={formData.subject} placeholder="Subject" className="my-5 h-10 p-3 bg-[#2b2b2b] text-slate-100"></input>
                     <textarea type="text" onChange={handleChange} name="message" value={formData.message} placeholder="Message" className="my-5 h-40 p-3 bg-[#2b2b2b] text-slate-100"></textarea>
                     <input type="submit" onClick={handleSubmit} value="Send Message!" className="border-2 self-end my-5 border-lime-600 text-slate-100 font-bold w-48 h-12 hover:bg-lime-600 duration-[600ms]"></input>
-                    <input type="hidden" name="form-name" value="contact" />
                 </form>
             </div>
         </div>
